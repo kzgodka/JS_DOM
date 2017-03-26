713110f7ebad4cdc65a00484091ad8cd5cfd4785
kzgodka <kamilzgodka@MacBook-Air-Kamil.local>
2017-03-26 13:45:56 +0200

    display innerText of buttons

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var withButtonClass = document.getElementsByClassName('button');
+	buttonsCount = withButtonClass.length;
+
+
+for (var i = 0; i < buttonsCount; i++) {
+	console.log(withButtonClass[i].innerText);
+}
+
